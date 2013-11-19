8e9d02109d0516e8f5284281cc8ef7fc2910b94c
Bureau54 GmbH <rm@bureau54.com>
2013-11-19 16:01:52 +0100

    alert fix

--- www/js/index.js
+++ www/js/index.js
@@ -59,16 +59,11 @@ var app = {
 
         scanner.scan( function (result) { 
 
-            alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
-
            console.log("Scanner result: \n" +
                 "text: " + result.text + "\n" +
                 "format: " + result.format + "\n" +
                 "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
+            //document.getElementById("info").innerHTML = result.text;
             console.log(result);
 		
 			
@@ -81,9 +76,9 @@ var app = {
 	            }}); 
 			
             
-            if (args.format == "QR_CODE") {
-                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-            }
+            //if (args.format == "QR_CODE") {
+            //    window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+            //}
             
 
         }, function (error) { 
